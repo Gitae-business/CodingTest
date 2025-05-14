07d1b8286e50c5a29b5d3d2766fcf424822de2c5
girae <gitae990723@gmail.com>
2025-05-14 18:16:21 +0900

    .

---
+++ BJ_3053.py
@@ -0,0 +1,10 @@
+# 택시 기하학 https://www.acmicpc.net/problem/3053
+import math
+
+def main():
+    r = int(input())
+    print(r * r * math.pi)
+    print(r * r * 2)
+
+if __name__ == '__main__':
+    main()
